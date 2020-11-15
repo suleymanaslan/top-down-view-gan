ac6644a44ab17f1f01c6cd023cc0ffc5b5eb18b3
suleymanaslan <suleymanaslan15@gmail.com>
2020-11-16 00:07:22 +0300

    Update pgan.py

--- pgan.py
+++ pgan.py
@@ -1,19 +1,17 @@
 from model import Model
-from env import Env
+from observation_data import ObservationData
 
 model = Model(max_scale=5,
               steps_per_scale=int(5e3),
               lr=1e-3)
 
-env = Env(obs_buffer_size=25,
-          data_buffer_size=1024,
-          batch_size=16)
+obs_data = ObservationData(obs_buffer_size=21,
+                           data_buffer_size=4096,
+                           batch_size=16)
+obs_data.load()
 
 for step_i in range(int(50e3)):
-    if step_i % 30 == 0:
-        for _ in range(24):
-            obs_data = env.step()
     x, y = obs_data.get_sample()
     model.train_step(x, y)
 
-env.close()
+model.save()
