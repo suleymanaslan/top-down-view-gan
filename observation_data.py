8aa577df1bce8c1c20d6a37cf047d1b2b363b8e1
suleymanaslan <suleymanaslan15@gmail.com>
2020-11-15 22:28:13 +0300

    Numpy conversion

--- observation_data.py
+++ observation_data.py
@@ -6,37 +6,37 @@ from collections import deque
 class ObservationData:
     def __init__(self, obs_buffer_size, data_buffer_size, batch_size):
         self.gpu_device = torch.device("cuda:0")
-        self.cpu_device = torch.device("cpu:0")
         self.obs_buffer_size = obs_buffer_size
         self.data_buffer_size = data_buffer_size
         self.batch_size = batch_size
         self.obs_buffer = deque([], maxlen=self.obs_buffer_size)
-        self.data_x = torch.zeros((self.data_buffer_size, self.obs_buffer_size, 3, 128, 128),
-                                  dtype=torch.float32, device=self.cpu_device)
-        self.data_y = torch.zeros((self.data_buffer_size, 3, 128, 128),
-                                  dtype=torch.float32, device=self.cpu_device)
+        self.data_x = np.zeros((self.data_buffer_size, self.obs_buffer_size, 128, 128, 3), dtype=np.uint8)
+        self.data_y = np.zeros((self.data_buffer_size, 128, 128, 3), dtype=np.uint8)
         self.counter = 0
         self.cur_top_down_obs = None
         self._reset_buffer()
 
     def _reset_buffer(self):
         for _ in range(self.obs_buffer_size):
-            self.obs_buffer.append(torch.zeros((3, 128, 128), dtype=torch.float32, device=self.cpu_device))
+            self.obs_buffer.append(np.zeros((128, 128, 3), dtype=np.uint8))
         self.cur_top_down_obs = None
 
-    def _process_obs(self, obs):
-        return torch.tensor(obs, dtype=torch.float32, device=self.cpu_device).div_(255).permute(2, 0, 1)
+    def _to_torch(self, np_array, x=False):
+        if x:
+            return (torch.from_numpy(np_array) / 255.0).permute(0, 1, 4, 2, 3).to(self.gpu_device)
+        else:
+            return (torch.from_numpy(np_array) / 255.0).permute(0, 3, 1, 2).to(self.gpu_device)
 
     def _add_sample(self):
         data_ix = self.counter % self.data_buffer_size
-        self.data_x[data_ix] = torch.stack(list(self.obs_buffer), 0)
-        self.data_y[data_ix] = self._process_obs(self.cur_top_down_obs)
+        self.data_x[data_ix] = np.array(self.obs_buffer)
+        self.data_y[data_ix] = self.cur_top_down_obs
         self.counter += 1
 
     def append_obs(self, obs, top_down_obs, new_episode=False):
         if new_episode:
             self._reset_buffer()
-        self.obs_buffer.append(self._process_obs(obs))
+        self.obs_buffer.append(obs)
         if self.cur_top_down_obs is None:
             self.cur_top_down_obs = top_down_obs
         else:
@@ -45,13 +45,13 @@ class ObservationData:
         self._add_sample()
 
     def get_buffer(self):
-        return torch.stack(list(self.obs_buffer), 0)
+        return np.array(self.obs_buffer)
 
     def get_sample(self, batch_size=None):
         if batch_size is None:
             batch_size = self.batch_size
         available_data = min(self.counter, self.data_buffer_size)
         batch_ix = np.random.choice(available_data, batch_size)
-        batch_x = self.data_x[batch_ix].to(self.gpu_device)
-        batch_y = self.data_y[batch_ix].to(self.gpu_device)
-        return batch_x, batch_y
+        batch_x = self.data_x[batch_ix]
+        batch_y = self.data_y[batch_ix]
+        return self._to_torch(batch_x, x=True), self._to_torch(batch_y)
